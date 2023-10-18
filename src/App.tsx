f21a3b62c97f2634ed700f406058e0835f8e4ed5
SantiCombina <santicombina@gmail.com>
2023-10-18 13:35:00 -0300

    Added routes and toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
+import "react-toastify/dist/ReactToastify.css";
 import { Pokemon } from "./pages/Pokemon";
-import "react-toastify/dist/ReactToastify.css"
-import { ToastContainer } from "react-toastify";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Pokemon/>
-      <ToastContainer
-                position="top-center"
-                autoClose={5000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                theme="dark"
-      />
-    </>
+    <Routes>
+      <Route path="/" element={<Pokemon/>}/>
+    </Routes>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  // <React.StrictMode>
-    <App />
-  // </React.StrictMode>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="dark"
+                />
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 )
